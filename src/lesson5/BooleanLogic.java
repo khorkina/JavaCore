a60f984dfb26c380f6ee123e8751124a25280258
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-06-22 18:55:28 +0300

    Boolean logic

--- src/lesson5/BooleanLogic.java
+++ src/lesson5/BooleanLogic.java
@@ -1,2 +1,41 @@
-package lesson5;public class BooleanLogic {
+package lesson5;
+
+public class BooleanLogic {
+    public static void main(String[] args) {
+        //Сынок купи хлеб
+        boolean isboughtbread = true;
+        //Сынок купил молоко
+        boolean isboughtmilk = true;
+
+        //and - logic и -> если два true -> иначе false
+        boolean isboughtbreadadbmilk = isboughtbread && isboughtmilk;
+
+
+        if (isboughtbread && isboughtmilk) {
+            System.out.println("Молодец сынок, купил хлеб и молоко");
+        } else if (isboughtmilk) {
+            System.out.println("Молодец сынок, хотя бы купил молоко");
+        } else if (isboughtbread) {
+            System.out.println("Молодец сынок, хотя бы купил хлеб");
+        }
+        else {
+            System.out.println("Сынок не молодец, хлеба нет и молока нет");
+
+        }
+
+        //or
+        boolean isboughtmilkorbread = true || true;
+        //boolean isboughtmilkorbread = false || true;
+
+        //купио сладости или нет
+        //not - !
+        boolean isboughtcandy = false;
+
+        if (!isboughtcandy) {
+            System.out.println("Мама хвалит сына");
+
+        }
+
+    }
+
 }
