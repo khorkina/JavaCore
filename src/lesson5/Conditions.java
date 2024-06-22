c9732d1c679aea2fd6c7d608883ee9818ef27603
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-06-22 18:34:12 +0300

    Conditions

---
+++ src/lesson5/Conditions.java
@@ -0,0 +1,19 @@
+package lesson5;
+
+public class Conditions {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 6;
+
+        if (x > y) {
+            System.out.println("x больше, чем y");
+        } else if (x == y) {
+            System.out.println("x равен y");
+        } else {
+            System.out.println("y больше x");
+
+        }
+
+    }
+
+}
