31606e5c27c5690936a3413da9422ee20865b480
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-06-22 17:54:08 +0300

    Primitive char

---
+++ src/lesson3/Primitivechar.java
@@ -0,0 +1,19 @@
+package lesson3;
+
+public class Primitivechar {
+    public static void main(String[] args) {
+        //char 16 bit
+        char letter = 'Я' + 1;
+        System.out.println(letter);
+
+        char newline = '\n';
+        System.out.print('а');
+        System.out.print(newline);
+        System.out.print('b');
+        char tab = '\t';
+        char escape = '\\';
+        System.out.println(escape);
+
+    }
+
+}
