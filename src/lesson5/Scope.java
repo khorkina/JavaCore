a93ce1f0ffde7871631ff666ba67cc0c15e32c0b
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-06-24 21:18:25 +0300

    Scope

---
+++ src/lesson5/Scope.java
@@ -0,0 +1,17 @@
+package lesson5;
+
+public class Scope {
+    public static void main(String[] args) {
+        double priceCar = 10_000;
+
+        if (priceCar > 9_000) {
+            double oldPrice = priceCar;
+            priceCar *= 0.95;
+            System.out.println(oldPrice);
+        }
+
+        System.out.println(priceCar);
+
+    }
+
+}
