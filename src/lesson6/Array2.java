af0ccb4c379fcf88b3daaf9ffc699cf5628e7038
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-06-28 20:52:35 +0300

    Array2

--- src/lesson6/Array2.java
+++ src/lesson6/Array2.java
@@ -8,7 +8,9 @@ public class Array2 {
         int y = 8;
 
         int[] array = {x, y};
-        int[] array2 = array;
+        int[] array2 = new int[2];
+        array2[0] = array[0];
+        array2[1] = array[1];
         array2[0] = 12;
         System.out.println(Arrays.toString(array));
         System.out.println(Arrays.toString(array2));
