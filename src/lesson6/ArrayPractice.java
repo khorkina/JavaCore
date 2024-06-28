aa9152d441fdbbc36573de378fe0392ada298bd0
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-06-28 20:58:58 +0300

    Array Practice

---
+++ src/lesson6/ArrayPractice.java
@@ -0,0 +1,10 @@
+package lesson6;
+
+public class ArrayPractice {
+    public static void main(String[] args) {
+        char[] answer = {'D', 'O', 'H', 'A'};
+        System.out.println(answer);
+
+    }
+
+}
