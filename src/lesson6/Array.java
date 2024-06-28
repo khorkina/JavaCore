6afe2f0cd07bea5ec0c2cf611c07cd6b20ccd83e
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-06-28 20:41:49 +0300

    Array

---
+++ src/lesson6/Array.java
@@ -0,0 +1,20 @@
+package lesson6;
+
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+//        int[] array = new int [2];
+//        array[0] = x;
+//        array[1] = y;
+
+//        int[] array = new int[] {x, y};
+
+        int[] array = {x, y};
+        System.out.println(Arrays.toString(array));
+
+    }
+
+}
