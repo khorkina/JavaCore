d3f37da3f54f66619e94af35287096d7b694ac35
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-07-01 17:16:55 +0300

    Multi Array

--- src/lesson6/MultiArray.java
+++ src/lesson6/MultiArray.java
@@ -1,2 +1,28 @@
-package lesson6;public class MultiArray {
+package lesson6;
+
+import java.util.Arrays;
+
+public class MultiArray {
+    public static void main(String[] args) {
+        int[] group1 = {180, 178, 190};
+        int[] group2 = {164, 158, 181};
+        int[][] multiArray = {group1, group2};
+
+        upperFor:
+        for (int[] array : multiArray) {
+            for (int number : array) {
+                if (number == 178) {
+                    System.out.println("There is a person with 178 cm height");
+                    break upperFor;
+                } else {
+                    System.out.println("There isn't person with 178 cm height");
+
+                }
+
+            }
+
+        }
+
+    }
+
 }
