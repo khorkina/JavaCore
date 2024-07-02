a75481cf5383d206890138f035d5da8332404a32
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-07-02 22:31:36 +0300

    My String

---
+++ src/lesson7/MyString.java
@@ -0,0 +1,10 @@
+package lesson7;
+
+public class MyString {
+    public static void main(String[] args) {
+        char symbol = 'a';
+        String text = new String ("hey");
+        String text2 = new String ("hey");
+        System.out.println(text.equals(text2));
+    }
+}
