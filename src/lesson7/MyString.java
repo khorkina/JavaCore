612155b39f453efb6205e98023f81199899c2362
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-07-02 22:33:41 +0300

    My String

--- src/lesson7/MyString.java
+++ src/lesson7/MyString.java
@@ -6,5 +6,10 @@ public class MyString {
         String text = new String ("hey");
         String text2 = new String ("hey");
         System.out.println(text.equals(text2));
+
+        System.out.println(text);
+        System.out.println(text2);
+
     }
+
 }
