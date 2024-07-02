817ebe0305fc37458636a6a37b979052fd0d9aa4
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-07-02 22:23:26 +0300

    Continue

---
+++ src/lesson6/Continue.java
@@ -0,0 +1,14 @@
+package lesson6;
+
+public class Continue {
+    public static void main(String[] args) {
+        int[] results = {3, 5, 6, 2, 1};
+        for (int result : results) {
+            System.out.println(result);
+            if (result == 6) {
+                continue;
+            }
+            System.out.println("Down");
+        }
+    }
+}
