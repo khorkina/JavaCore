4726c1040aa146fa85e5150846cbee901766cd8f
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-07-03 21:20:41 +0300

    String Method

---
+++ src/lesson7/StringMethod.java
@@ -0,0 +1,13 @@
+package lesson7;
+
+public class StringMethod {
+    public static void main(String[] args) {
+        String text = "Hello, World";
+        String textUpper = text.toUpperCase();
+        System.out.println(textUpper);
+
+        String textLower = text.toLowerCase();
+        System.out.println(textLower);
+
+    }
+}
