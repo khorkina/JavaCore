3d42715f7f71062169b19e4a116477f774d779b9
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-07-03 21:47:05 +0300

    String Wethods

--- src/lesson7/StringMethod.java
+++ src/lesson7/StringMethod.java
@@ -1,5 +1,7 @@
 package lesson7;
 
+import java.util.Arrays;
+
 public class StringMethod {
     public static void main(String[] args) {
         String text = "Hello, World";
@@ -18,7 +20,30 @@ public class StringMethod {
         String replacement = text.replaceAll("r", "a");
         System.out.println(replacement);
 
+        String repeat = text.repeat(10);
+        System.out.println(repeat);
 
+        String names = "Petr, Ilya, Gosha";
+        String[] namesArrays = names.split(";");
+        System.out.println(Arrays.toString(namesArrays));
+
+        String word1 = "Hi";
+        String word2 = "Friends";
+        String resultWord = word1 + word2;
+        System.out.println(resultWord);
+
+        boolean hel = text.startsWith("Hel");
+        System.out.println(hel);
+
+        boolean world = text.endsWith("World");
+        System.out.println(world);
+
+        String first5Letters = text.substring(0, 5);
+        System.out.println(first5Letters);
+
+        int lengthText2 = "Hello".replaceAll("l", "a").toUpperCase().length();
+        System.out.println(lengthText2);
 
     }
+
 }
