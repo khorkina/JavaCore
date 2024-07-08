096597565572b5c40d7df4d2ff0106f27abd301b
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-07-08 19:36:07 +0300

    Text Block

--- src/lesson7/TextBlock.java
+++ src/lesson7/TextBlock.java
@@ -2,10 +2,13 @@ package lesson7;
 
 public class TextBlock {
     public static void main(String[] args) {
+        String name1 = "Anna";
+        String name2 = "Maina";
         String text = """
                 Hi, what is your name?
-                My name is Marina.
-                """;
+                My name is %s. And you?
+                My name is %s.
+                """.formatted(name1, name2);
         System.out.println(text);
 
     }
