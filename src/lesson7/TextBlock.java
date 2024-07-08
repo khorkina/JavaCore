4878eda503509e6807688b63c5e895cb42c55d1f
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-07-08 19:31:35 +0300

    Text Block

---
+++ src/lesson7/TextBlock.java
@@ -0,0 +1,13 @@
+package lesson7;
+
+public class TextBlock {
+    public static void main(String[] args) {
+        String text = """
+                Hi, what is your name?
+                My name is Marina.
+                """;
+        System.out.println(text);
+
+    }
+
+}
