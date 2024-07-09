467c34daa181f9c9045430a50469cdba1f50c2c3
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-07-09 22:21:54 +0300

    Runner

--- src/lesson8/Runner.java
+++ src/lesson8/Runner.java
@@ -1,2 +1,8 @@
-package lesson8;public class Runner {
+package lesson8;
+
+public class Runner {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        System.out.println(cat);
+    }
 }
