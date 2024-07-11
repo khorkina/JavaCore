ed29f0faac460c22906adc7ebe112092ddd69969
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-07-11 20:44:51 +0300

    Runner

--- src/lesson8/Runner.java
+++ src/lesson8/Runner.java
@@ -2,7 +2,7 @@ package lesson8;
 
 public class Runner {
     public static void main(String[] args) {
-        Cat cat = new Cat();
+        Cat cat = new Cat("Black", 45, "Marina");
         System.out.println(cat);
 
     }
