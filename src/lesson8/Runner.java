5cd0e326df71e04fe6f5d64057cbc9ab4b36c898
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-07-13 20:56:44 +0300

    Runner

--- src/lesson8/Runner.java
+++ src/lesson8/Runner.java
@@ -3,7 +3,9 @@ package lesson8;
 public class Runner {
     public static void main(String[] args) {
         Cat cat = new Cat("Black", 45, "Marina");
-        System.out.println(cat);
+        System.out.println(cat.color);
+        cat.color = "Red";
+        System.out.println(cat.color);
 
     }
 
