728913c85cd5f10a555583430f20c6dbd7b623f3
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-07-15 22:24:58 +0300

    Tusk

--- src/lesson8/Tusk.java
+++ src/lesson8/Tusk.java
@@ -1,2 +1,10 @@
-package lesson8;public class Tusk {
+package lesson8;
+
+public class Tusk {
+    public static void main(String[] args) {
+        Book book = new Book("Blue Latern", "Pelevin", 230, false);
+        book.printCountRows();
+        book.printCountRows(38);
+        book.printInfo();
+    }
 }
