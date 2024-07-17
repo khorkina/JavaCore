4e29f89d883fa1a63f89718139c731428aa6461e
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-07-17 22:05:11 +0300

    Recursion

--- src/lesson8/Recursion.java
+++ src/lesson8/Recursion.java
@@ -1,2 +1,18 @@
-package lesson8;public class Recursion {
+package lesson8;
+
+public class Recursion {
+    public int sum(int number) {
+        if (number < 1) {
+            return 0;
+        }
+        int result = number + sum(number - 1);
+        return result;
+    }
+
+    //number = 10 -> result = 10 + sum(10 - 1)
+    //number = 9 -> result = 9 + sum(9 - 1)
+    //...
+    //result = 2 -> result = 2 + 1
+    //number = 1 -> result = 1
+    //number = 0 -> return 0
 }
