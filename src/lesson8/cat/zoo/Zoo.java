bfc87324df85ac6152051a2fe40967288a3cf0a1
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-07-24 19:32:00 +0300

    Zoo

--- src/lesson8/cat/zoo/Zoo.java
+++ src/lesson8/cat/zoo/Zoo.java
@@ -1,2 +1,11 @@
-package lesson8.zoo;public class Zoo {
+package lesson8.cat.zoo;
+
+import lesson8.cat.Cat;
+
+public class Zoo {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Black", 20, "Marina");
+        System.out.println(cat.color);
+
+    }
 }
