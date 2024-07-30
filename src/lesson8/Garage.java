2ae23048de8834f5085c6f9b54ac2b94c86e10e0
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-07-30 23:16:15 +0300

    Garage

--- src/lesson8/Garage.java
+++ src/lesson8/Garage.java
@@ -1,2 +1,27 @@
-package lesson8;public class Garage {
+package lesson8;
+
+public class Garage {
+    private Car car1;
+    private Car car2;
+
+    public Garage() {
+    }
+
+    public void setCar1(Car car1) {
+        this.car1 = car1;
+    }
+
+    public void setCar2(Car car2) {
+        this.car2 = car2;
+    }
+
+    public void getInfoAboutCars() {
+        getInfoAboutCar(car1);
+        getInfoAboutCar(car2);
+    }
+
+    private void getInfoAboutCar(Car car) {
+        System.out.println(car.getBrandName());
+        car.investigateSpeed100();
+    }
 }
