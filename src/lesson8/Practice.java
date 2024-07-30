9e658cad42cfcf0cff9a1983607a84042688c8a6
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-07-30 23:17:34 +0300

    Practice

--- src/lesson8/Practice.java
+++ src/lesson8/Practice.java
@@ -1,2 +1,13 @@
-package lesson8;public class Practice {
+package lesson8;
+
+public class Practice {
+    public static void main(String[] args) {
+        Car marinaCar = new Car("MarinaCar", 400);
+        marinaCar.investigateSpeed100();
+
+        Garage garage = new Garage();
+        garage.setCar1(marinaCar);
+        garage.getInfoAboutCars();
+
+    }
 }
