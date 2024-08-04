29ab055d67298cd69954e5ff8f0dca3eca32e7dc
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-08-04 23:37:50 +0300

    Car

--- src/lesson9/Car.java
+++ src/lesson9/Car.java
@@ -2,8 +2,17 @@ package lesson9;
 
 public class Car {
     private String brand;
+    private static int countCars = 0;
 
     public Car(String brand) {
         this.brand = brand;
+        countCars++;
+
     }
+
+    public int getCountCars() {
+        return countCars;
+
+    }
+
 }
