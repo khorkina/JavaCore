ea36912d39fedcd38cb080816d6ce26b1ec5a109
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-08-04 23:39:08 +0300

    Runnar

--- src/lesson9/Runner.java
+++ src/lesson9/Runner.java
@@ -1,2 +1,12 @@
-package lesson9;public class Runner {
+package lesson9;
+
+public class Runner {
+    public static void main(String[] args) {
+        Car audi = new Car("Audi");
+        System.out.println(audi.getCountCars());
+        Car bmw = new Car("BMW");
+        System.out.println(bmw.getCountCars());
+
+    }
+
 }
