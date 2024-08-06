16a2b16af1e5a3263f7c4a24e38fca48b1951513
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-08-06 21:27:35 +0300

    Car

--- src/lesson9/Car.java
+++ src/lesson9/Car.java
@@ -3,6 +3,8 @@ package lesson9;
 public class Car {
     private String brand;
     private static int countCars = 0;
+    public final static String BMW_INFO = "BMW is a german company. It was created in 1928 year.";
+
 
     public Car(String brand) {
         this.brand = brand;
