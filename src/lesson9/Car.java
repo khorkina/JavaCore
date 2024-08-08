f8f6c3f4a3eaa5e0310bbf8ed880acbb33c1bcec
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-08-08 22:12:06 +0300

    Car

--- src/lesson9/Car.java
+++ src/lesson9/Car.java
@@ -4,6 +4,15 @@ public class Car {
     private String brand;
     private static int countCars = 0;
     public final static String BMW_INFO = "BMW is a german company. It was created in 1928 year.";
+    public final static String AUDI_INFO = "Audi is a german company. It was created in 1933 year.";
+    public final static String KIA_INFO = "KIA is a korean company. It was created in 1945 year.";
+    public final static String[] BRAND_DESCRIPTIONS = new String[3];
+
+    static {
+        BRAND_DESCRIPTIONS[0] = BMW_INFO;
+        BRAND_DESCRIPTIONS[1] = AUDI_INFO;
+        BRAND_DESCRIPTIONS[2] = KIA_INFO;
+    }
 
 
     public Car(String brand) {
