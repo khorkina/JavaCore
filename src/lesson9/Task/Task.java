2100ad440e13c08c882511f993bc02fa9b82db05
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-08-11 19:38:33 +0300

    Task

--- src/lesson9/Task/Task.java
+++ src/lesson9/Task/Task.java
@@ -1,2 +1,13 @@
-package lesson9.Task;public class Task {
+package lesson9.Task;
+
+public class Task {
+    public static void main(String[] args) {
+        Restaurant restaurant1 = new Restaurant();
+        Restaurant restaurant2 = new Restaurant();
+        restaurant1.cookHotDog();
+        restaurant2.addProducts();
+        restaurant2.cookHotDog();
+
+    }
+
 }
