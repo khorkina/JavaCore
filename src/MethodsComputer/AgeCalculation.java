67cc606a6a9bd5a9093de7d9ba609c66c346472c
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-09-04 22:00:44 +0300

    Age Calculation

--- src/MethodsComputer/AgeCalculation.java
+++ src/MethodsComputer/AgeCalculation.java
@@ -1,21 +1,14 @@
 package MethodsComputer;
 
 public class AgeCalculation {
-    public static void main(String[] args) {
-        int maxAgeNow = 0;
-        for (int m = 10; m < 100; m++) {
-            int a = m / 10;
-            int b = m % 10;
-            maxAgeNow = a + b;
 
-
-            int mFuture = m + 5;
-            int maxAgeFuture = maxAgeNow + 5;
-            if (maxAgeFuture == digitsProduct(mFuture)) {
-                break;
-            }
+    public static int digitsSum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
         }
-        System.out.println("Max's age now is: " + maxAgeNow);
+        return sum;
     }
 
     public static int digitsProduct(int num) {
@@ -25,6 +18,25 @@ public class AgeCalculation {
             num /= 10;
         }
         return product;
+    }
+
+    public static void findAges() {
+        for (int m = 10; m < 100; m++) {
+            int maxAgeNow = digitsSum(m);
+            int maxAgeInFiveYears = maxAgeNow + 5;
+
+            int minFiveYears = m + 5;
+            int productOfDigits = digitsProduct(minFiveYears);
+
+            if (maxAgeInFiveYears == productOfDigits) {
+                System.out.printf("Сегодня Максу %d лет, а Морицу %d лет.%n", maxAgeNow, m);
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        findAges();
     }
 }
 
