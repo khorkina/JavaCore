92ef5e49c770d6d12bd83fc4b646f0011ddaa390
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-09-06 22:12:00 +0300

    Mobile

---
+++ src/MethodsComputer/Mobile.java
@@ -0,0 +1,49 @@
+package MethodsComputer;
+
+class Mobile{
+    String brand;
+    int price;
+    String network;
+    //	String name;
+    static String name;
+
+    public void show() {
+        System.out.println(brand+" : "+price+" : "+name);
+    }
+
+    public static void show1(Mobile obj)
+    {
+//		System.out.println("in static method");
+        System.out.println(obj.brand+" : "+ obj.price +" : "+obj.name);
+    }
+
+    public static class Demo01 {
+        public static void main(String[] args)
+        {
+            Mobile obj1=new Mobile();
+            obj1.brand="Apple";
+            obj1.price=1500;
+            //obj1.name="SmartPhone";
+            name ="SmartPhone";
+
+            Mobile obj2=new Mobile();
+            obj2.brand="Samsung";
+            obj2.price=1700;
+            //obj2.name="SmartPhone";
+            name ="SmartPhone";
+
+            //obj1.name="Phone";
+            name ="SmartPhone";
+
+            obj1.show();
+            obj2.show();
+
+            show1(obj1);
+
+            //System.out.println(obj1.brand);
+
+
+        }
+    }
+}
+
