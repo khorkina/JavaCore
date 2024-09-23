63bd803c94e8cc27b44d3de93fa1744f6f4f4b0c
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-09-23 21:44:37 +0300

    Demo

--- src/MethodsComputer/HumanDemo.java
+++ src/MethodsComputer/HumanDemo.java
@@ -1,2 +1,15 @@
-package MethodsComputer;public class HumanDemo {
+package MethodsComputer;
+
+public class HumanDemo {
+    public static void main(String[] args) throws ClassNotFoundException
+    {
+        Human obj=new Human();
+        obj.SetAge(30);
+        obj.setName("Reddy");
+//		obj.age=11;
+//		obj.name="Navin";
+
+        System.out.println(obj.getName()+" : "+obj.getAge());
+
+    }
 }
