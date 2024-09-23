049a1e51d3d90c4fe98014d2f33af32210c0c120
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-09-23 21:43:50 +0300

    Human

---
+++ src/MethodsComputer/Human.java
@@ -0,0 +1,30 @@
+package MethodsComputer;
+
+class Human
+{
+    //int age;
+    //private int age=11;
+    private int age;
+    //String name;
+    //private String name="Navin";
+    private String name;
+
+    public int getAge()
+    {
+        return age;
+    }
+    public void SetAge(int a)
+    {
+        age=a;
+    }
+
+
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String n)
+    {
+        name=n;
+    }
+}
---
+++ src/MethodsComputer/HumanDemo.java
@@ -0,0 +1,2 @@
+package MethodsComputer;public class HumanDemo {
+}
