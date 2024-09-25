52b1c08cb0139588bb14768b4ba7790877bc0676
Marina Khorkina <166530415+khorkina@users.noreply.github.com>
2024-09-25 21:20:05 +0300

    Calc

---
+++ src/AdvCalc.java
@@ -0,0 +1,27 @@
+class Calc
+{
+    public int add(int n1, int n2)
+    {
+        return n1+n2;
+    }
+    public int sub(int n1, int n2)
+    {
+        return n1-n2;
+    }
+}
+
+
+public class AdvCalc extends Calc
+{
+    public int multi(int n1, int n2)
+    {
+        return n1*n2;
+    }
+    public int div(int n1, int n2)
+    {
+        return n1/n2;
+    }
+}
+
+
+
